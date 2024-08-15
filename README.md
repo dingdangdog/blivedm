836775e606682e6f5ea83518462988de69414abd
dddx <dingdangdogx@outlook.com>
2024-08-15 14:34:27 +0800

    feat: 新增屏蔽用户和屏蔽词逻辑，并优化代码

--- README.md
+++ README.md
@@ -12,12 +12,18 @@ Execute `pip install pyinstaller` to install `pyinstaller`, and then execute the
 # pyinstaller --onefile blivemd-voice.py
 
 # windows azure
-pyinstaller --onefile --add-data "./.venv/Lib/site-packages/azure;azure" blivemd-voice.py
+# pyinstaller --onefile --add-data "./.venv/Lib/site-packages/azure;azure" blivemd-voice.py
+
+pyinstaller --onefile --add-data "C:/Users/ddd/AppData/Roaming/Python/Python310/site-packages/azure;azure" blivemd-voice.py
 ```
 
 ## config
 
-文件名：`config.json`，默认配置：
+文件名：`config.json`，该配置文件负责系统主要行为的配置，包括弹幕/礼物/点赞等语音控制。
+
+基本参数配置教程可前往B站观看视频：[B站直播弹幕语音播报机器人-使用教程](https://www.bilibili.com/video/BV1St421b7Ht/)
+
+最新版默认配置如下：
 
 ```json
 {
@@ -36,96 +42,44 @@ pyinstaller --onefile --add-data "./.venv/Lib/site-packages/azure;azure" blivemd
     "like_total": "本次直播点赞数量超过 {limit_num} 次，达到 {click_count} 次"
   },
   "like_nums": [66, 188, 300, 500, 666, 888, 999, 1666],
-  "max_next_interval": 100
+  "max_next_interval": 100,
+  "black_user": ["屏蔽用户1", "屏蔽用户2"],
+  "black_text": ["屏蔽词1", "屏蔽词2"]
 }
 ```
 
-### platform
+### 基本配置说明
 
-> 目前仅支持windows。
+1. `platform`：目前仅支持windows。win
+2. `mode`：可选配置（select）: local (`default`)/azure/alibaba/sovits
+3. `bilibili_SESSION`：这里应该写一个已登录的`SESSDATA`，你可以在浏览器`cookie`中获取它。不填写也可以连接，但是不会获取用户名和id。
+4. `bilibili_heart_print`：心跳监听输出间隔，默认值：`10`。
+5. `welcome_level`：用户粉丝牌等级， 默认值：`0`。作用：进场提示音等级控制，粉丝牌大于等于指定等级的用户进入房间，才会用欢迎语音和弹幕提示。
+6. `continuous_gift_interval`：秒，默认值：`1`。作用：用于配置连续礼物的时间间隔，时间间隔内的相同礼物将会合并播报。
+7. `voice_text`：用于配置常用的语音文本。    默认配置（default）：
 
-可选配置（select）:
+    ```json
+    {
+        "enter": "欢迎 {uname} 进入直播间，记得常来玩哦！",
+        "danmaku": "{uname}说：{msg}",
+        "gift": "感谢 {uname} 赠送的 {num}个{gift_name}，谢谢老板，老板大气！",
+        "like": "感谢 {uname} {like_text}",
+        "like_total": "本次直播点赞数量达到 {click_count} 次"
+    }
+    ```
 
-- win (`default`)
-- mac
-- linux
+    详细说明：
 
-### mode
+    - `enter`：进入直播间的语音文字，`uname` 会自动替换为用户昵称；
+    - `danmaku`：弹幕播报的语音文字，`uname`-用户昵称、`msg`-弹幕内容；
+    - `gift`：礼物播报的语音文字，`uname`-用户昵称、`num`-礼物个数、`gift_name`-礼物名称；
+    - `like`：用户点赞的语音文字，`uname`-用户昵称、`like_text`-B站官方点赞提示文字；
+    - `like_total`：点赞总结的语音文字，`click_count`-总点赞次数。
 
-可选配置（select）:
-
-- local (`default`)
-- azure
-- alibaba
-- sovits
-
-### bilibili_SESSION
-
-这里应该写一个已登录的`SESSDATA`，你可以在`cookie`中获取它。
-不填写也可以连接，但是不会获取用户名和id。
-
-There should to write a logged in `SESSDATA`, you can get it in `cookie`.
-You can connect without filling it in, but username and id will not be obtained.
-
-### bilibili_heart_print
-
-- 含义：心跳监听次数
-- 默认值：`10`
-- 作用：心跳监控信息打印心跳次数间隔。
-
-Heartbeat monitoring information printing interval.
-
-### welcome_level
-
-- 含义：用户粉丝牌等级
-- 默认值：`0`
-- 作用：进场提示音等级控制，粉丝牌大于等于指定等级的用户进入房间，才会用欢迎语音和弹幕提示
-
-### continuous_gift_interval
-
-- 含义：秒
-- 默认值：`1`
-- 作用：用于配置连续礼物的时间间隔，时间间隔内的相同礼物将会合并播报。
-
-Used to configure the time interval for consecutive gifts. The same gifts within the time interval will be combined and broadcast.
-
-### voice_text
-
-用于配置常用的语音文本。
-
-Used to configure commonly used voice texts.
-
-默认配置（default）：
-
-```json
-{
-    "enter": "欢迎 {uname} 进入直播间，记得常来玩哦！",
-    "danmaku": "{uname}说：{msg}",
-    "gift": "感谢 {uname} 赠送的 {num}个{gift_name}，谢谢老板，老板大气！",
-    "like": "感谢 {uname} {like_text}",
-    "like_total": "本次直播点赞数量达到 {click_count} 次"
-}
-```
-
-配置详细说明：
-
-- enter：进入直播间的语音文字，`uname` 会自动替换为用户昵称；
-- danmaku：弹幕播报的语音文字，`uname`-用户昵称、`msg`-弹幕内容；
-- gift：礼物播报的语音文字，`uname`-用户昵称、`num`-礼物个数、`gift_name`-礼物名称；
-- like：用户点赞的语音文字，`uname`-用户昵称、`like_text`-B站官方点赞提示文字；
-- like_total：点赞总结的语音文字，`click_count`-总点赞次数。
-
-### like_nums（待测试）
-
-- 含义：点赞数量数组
-- 默认值：`[66, 188, 300, 500, 666, 888, 999, 1666]`
-- 作用：指定需要语音播报的点赞数量，当低于最小值时，语音不会做任何播报
-
-### max_next_interval
-
-- 含义：点赞超过上限后的递增数量
-- 默认值：`100`
-- 作用：点赞数量超过设定数组 `like_nums` 的最大值后，后续语音播报与上次语音播报的间隔
+8. `like_nums`：点赞数量数组，默认值：`[66, 188, 300, 500, 666, 888, 999, 1666]`。作用：指定需要语音播报的点赞数量，当低于最小值时，语音不会做任何播报。
+9. `max_next_interval`：点赞超过上限后的递增数量，默认值：`100`。作用：点赞数量超过设定数组 `like_nums` 的最大值后，后续语音播报与上次语音播报的间隔。
+10. `black_user`：黑名单用户数组，全名！配置其中的用户行为将不被播报，包括：进场/弹幕/礼物/点赞。
+11. `black_text`：屏蔽词数组，模糊匹配！弹幕内容包含屏蔽词中任意一个时，则不会播报。
 
 ## azure_config
 
@@ -140,45 +94,45 @@ Used to configure commonly used voice texts.
 }
 ```
 
-### azure_model
+### azure配置说明
 
-[语音服务的语言和声音支持](https://learn.microsoft.com/zh-cn/azure/ai-services/speech-service/language-support?tabs=tts)
+1. `azure_key`：Azure平台的私钥。
+2. `azure_model`：[语音服务的语言和声音支持](https://learn.microsoft.com/zh-cn/azure/ai-services/speech-service/language-support?tabs=tts)，常用中文模型（2024年1月记录）：
 
-[Language and voice support for the Speech service](https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=tts)
+    ```json
+    {
+    "zh-CN-XiaoxiaoNeural": "（女）(default)",
+    "zh-CN-YunxiNeural": "（男）",
+    "zh-CN-YunjianNeural": "（男）",
+    "zh-CN-XiaoyiNeural": "（女）",
+    "zh-CN-YunyangNeural": "（男）",
+    "zh-CN-XiaochenNeural": "（女）",
+    "zh-CN-XiaohanNeural": "（女）",
+    "zh-CN-XiaomengNeural": "（女）",
+    "zh-CN-XiaomoNeural": "（女）",
+    "zh-CN-XiaoqiuNeural": "（女）",
+    "zh-CN-XiaoruiNeural": "（女）",
+    "zh-CN-XiaoshuangNeural": "（女性、儿童）",
+    "zh-CN-XiaoxuanNeural": "（女）",
+    "zh-CN-XiaoyanNeural": "（女）",
+    "zh-CN-XiaoyouNeural": "（女性、儿童）",
+    "zh-CN-XiaozhenNeural": "（女）",
+    "zh-CN-YunfengNeural": "（男）",
+    "zh-CN-YunhaoNeural": "（男）",
+    "zh-CN-YunxiaNeural": "（男）",
+    "zh-CN-YunyeNeural": "（男）",
+    "zh-CN-YunzeNeural": "（男）",
+    "zh-CN-XiaochenMultilingualNeural1": "（女）",
+    "zh-CN-XiaorouNeural1": "（女）",
+    "zh-CN-XiaoxiaoDialectsNeural1": "（女）",
+    "zh-CN-XiaoxiaoMultilingualNeural1": "（女）no voice？",
+    "zh-CN-YunjieNeural1": "（男）",
+    "zh-CN-YunyiMultilingualNeural1": "（男）"
+    }
+    ```
 
-- 中文模型（2024年1月记录）：
-
-```json
-{
- "zh-CN-XiaoxiaoNeural": "（女）(default)",
- "zh-CN-YunxiNeural": "（男）",
- "zh-CN-YunjianNeural": "（男）",
- "zh-CN-XiaoyiNeural": "（女）",
- "zh-CN-YunyangNeural": "（男）",
- "zh-CN-XiaochenNeural": "（女）",
- "zh-CN-XiaohanNeural": "（女）",
- "zh-CN-XiaomengNeural": "（女）",
- "zh-CN-XiaomoNeural": "（女）",
- "zh-CN-XiaoqiuNeural": "（女）",
- "zh-CN-XiaoruiNeural": "（女）",
- "zh-CN-XiaoshuangNeural": "（女性、儿童）",
- "zh-CN-XiaoxuanNeural": "（女）",
- "zh-CN-XiaoyanNeural": "（女）",
- "zh-CN-XiaoyouNeural": "（女性、儿童）",
- "zh-CN-XiaozhenNeural": "（女）",
- "zh-CN-YunfengNeural": "（男）",
- "zh-CN-YunhaoNeural": "（男）",
- "zh-CN-YunxiaNeural": "（男）",
- "zh-CN-YunyeNeural": "（男）",
- "zh-CN-YunzeNeural": "（男）",
- "zh-CN-XiaochenMultilingualNeural1": "（女）",
- "zh-CN-XiaorouNeural1": "（女）",
- "zh-CN-XiaoxiaoDialectsNeural1": "（女）",
- "zh-CN-XiaoxiaoMultilingualNeural1": "（女）no voice？",
- "zh-CN-YunjieNeural1": "（男）",
- "zh-CN-YunyiMultilingualNeural1": "（男）"
-}
-```
+3. `azure_region`：Azure平台地区编码
+4. `azure_endpoint`：Azure平台接口地址
 
 ## config_alibaba
 
@@ -195,12 +149,14 @@ Used to configure commonly used voice texts.
 }
 ```
 
+### alibaba配置说明
+
 `alibaba` 智能语音交互的配置如上所示，参数介绍：
 
-- `alibaba_appkey`：智能语音交互创建项目后，项目的`appkey`；
-- `alibaba_token`：目前仅支持通过控制台获取临时token，临时token有效时间为24小时，失效后需要重新获取，获取方式可参考：[阿里云文档](https://help.aliyun.com/zh/isi/getting-started/obtain-an-access-token-in-the-console)；
-- `alibaba_model`：参考官方文档：[语音合成-接口说明](https://help.aliyun.com/zh/isi/developer-reference/overview-of-speech-synthesis)；
-- `alibaba_endpoint`：阿里云接口节点，有上海`shanghai`、北京`beijing`、深圳`shenzhen`三个节点，自行修改即可。
+1. `alibaba_appkey`：智能语音交互创建项目后，项目的`appkey`；
+2. `alibaba_token`：目前仅支持通过控制台获取临时token，临时token有效时间为24小时，失效后需要重新获取，获取方式可参考：[阿里云文档](https://help.aliyun.com/zh/isi/getting-started/obtain-an-access-token-in-the-console)；
+3. `alibaba_model`：参考官方文档：[语音合成-接口说明](https://help.aliyun.com/zh/isi/developer-reference/overview-of-speech-synthesis)；
+4 `alibaba_endpoint`：阿里云接口节点，有上海`shanghai`、北京`beijing`、深圳`shenzhen`三个节点，自行修改即可。
 
 ## config_soVits
 
--- blivemd-voice.py
+++ blivemd-voice.py
@@ -29,14 +29,16 @@ VOICE_TEXT: {} = {}
 LIKE_NUMS: [] = [100, 500, 666, 1000]
 LIKE_NEXT_INTERVAL: int = 100
 WELCOME_LEVEL: int = 0
+BLACK_USER: [] = []
+BLACK_TEXT: [] = []
 
 
 async def main():
     init_config()
     init_session()
-    text = f'语音机器人启动成功，监控直播间：{ROOM_IDS}，语音模式：{MODE}，心跳间隔：{HEART_PRINT}，连续礼物间隔：{GIFT_INTERVAL}'
+    text = f'启动成功，直播间：{ROOM_IDS}，语音模式：{MODE}，心跳间隔：{HEART_PRINT}，连续礼物间隔：{GIFT_INTERVAL}秒'
     print(text)
-    speech(text)
+    # speech(text)
     try:
         await run_single_client()
         await run_multi_clients()
@@ -55,6 +57,8 @@ def init_config():
     global LIKE_NUMS
     global LIKE_NEXT_INTERVAL
     global WELCOME_LEVEL
+    global BLACK_USER
+    global BLACK_TEXT
     config = read_json_config("config.json")
     PLATFORM = config['platform']
     MODE = config['mode']
@@ -66,6 +70,8 @@ def init_config():
     LIKE_NUMS = config['like_nums']
     LIKE_NEXT_INTERVAL = config['max_next_interval']
     WELCOME_LEVEL = config['welcome_level']
+    BLACK_USER = config['black_user']
+    BLACK_TEXT = config['black_text']
     if MODE == 'azure':
         init_azure_config()
     if MODE == 'alibaba':
@@ -148,8 +154,10 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
     def _on_enter(self, client: blivedm.BLiveClient, data: web_models.UserInData):
         if data.medal_level >= WELCOME_LEVEL:
             print(f' {data.uname} 进入直播间了')
-            voice_text = VOICE_TEXT["enter"].format(uname=data.uname)
-            speech(voice_text)
+            if (len(VOICE_TEXT["enter"]) > 0):
+                if (data.uname not in BLACK_USER):
+                    voice_text = VOICE_TEXT["enter"].format(uname=data.uname)
+                    speech(voice_text)
 
     # 弹幕消息
     def _on_danmaku(self, client: blivedm.BLiveClient, message: web_models.DanmakuMessage):
@@ -160,9 +168,21 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
         except (ValueError, TypeError):
             msg = msg
         finally:
-            voice_text = VOICE_TEXT["danmaku"].format(uname=message.uname, msg=msg)
-            speech(voice_text)
-
+            if (len(VOICE_TEXT["enter"]) > 0):
+                isBlackText = False
+                blackText = ""
+                for word in BLACK_TEXT:
+                    if (msg.find(word) >= 0):
+                        isBlackText = True
+                        blackText = word
+                        break
+                if(isBlackText):
+                    print(f'   屏蔽词：{blackText}')
+                elif (message.uname in BLACK_USER):
+                    print(f'   黑名单用户发言：{message.uname}')
+                else:
+                    voice_text = VOICE_TEXT["danmaku"].format(uname=message.uname, msg=msg)
+                    speech(voice_text)
     # 特殊弹幕通知
     def _on_spacial_danmaku(self, client: blivedm.BLiveClient, message: web_models.SpacialDanMaku):
         # 使用 for 循环输出 content_segments 中的 text 属性
@@ -175,10 +195,10 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
         # 执行相应的逻辑
         print(f' {message.uname} 赠送 {message.gift_name}x{message.num}'
               f' （价值x{message.price}）')
-
-        voice_text = VOICE_TEXT["gift"].format(uname=message.uname, num=message.num,
-                                               gift_name=message.gift_name)
-        speech(voice_text)
+        if (len(VOICE_TEXT["gift"]) > 0 and message.uname not in BLACK_USER):
+            voice_text = VOICE_TEXT["gift"].format(uname=message.uname, num=message.num,
+                                                gift_name=message.gift_name)
+            speech(voice_text)
 
     # 礼物信息
     def _on_gift(self, client: blivedm.BLiveClient, message: web_models.GiftMessage):
@@ -211,9 +231,10 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
                         print(f' {uname} 赠送 {message.gift_name}x{message.num}'
                               f' （{message.coin_type}瓜子x{message.total_coin}）')
 
-                        voice_text = VOICE_TEXT["gift"].format(uname=uname, num=message.num,
-                                                               gift_name=message.gift_name)
-                        speech(voice_text)
+                        if (len(VOICE_TEXT["gift"]) > 0 and message.uname not in BLACK_USER):
+                            voice_text = VOICE_TEXT["gift"].format(uname=uname, num=message.num,
+                                                                gift_name=message.gift_name)
+                            speech(voice_text)
                 # 清空队列
                 self.gift_queue.queue.clear()
             # 更新上一次队列的大小
@@ -222,13 +243,15 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
     # 舰长？
     def _on_buy_guard(self, client: blivedm.BLiveClient, message: web_models.GuardBuyMessage):
         print(f' {message.username} 购买 {message.gift_name}')
+        speech(f' 感谢 {message.username} 购买 {message.gift_name}')
 
     # 点赞消息处理：PS：可能存在并发问题
     def _click_like(self, client: blivedm.BLiveClient, data: web_models.ClickData):
         if len(data.uname) != 0:
             print(f' {data.uname} {data.like_text}')
-            voice_text = VOICE_TEXT["like"].format(uname=data.uname, like_text=data.like_text)
-            speech(voice_text)
+            if (len(VOICE_TEXT["like"]) > 0 and data.uname not in BLACK_USER):
+                voice_text = VOICE_TEXT["like"].format(uname=data.uname, like_text=data.like_text)
+                speech(voice_text)
         else:
             if LIKE_NUMS[0] > data.click_count:
                 return
@@ -242,7 +265,8 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
                     self.last_click_like_num = data.click_count
                     text = f' 太棒了！本次直播点赞数量超过设定最大值 {LIKE_NUMS[len(LIKE_NUMS) - 1]} 次，达到 {data.click_count} 次'
                     print(text)
-                    speech(text)
+                    if (len(VOICE_TEXT["like"]) > 0):
+                        speech(text)
                 else:
                     # 后续超过最大值后，按照上次播报后的数量100递增提示，每超过100次播报一次。
                     # 如：上一次播报的是1909次，则下次将会在超过2009次时播报，否则不播报
@@ -250,7 +274,8 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
                     # 点赞数量更新，本场直播的总点赞数量
                     text = f' 点赞数量达到 {data.click_count} 次'
                     print(text)
-                    speech(text)
+                    if (len(VOICE_TEXT["like"]) > 0):
+                        speech(text)
                 return
 
             while True:
@@ -261,9 +286,11 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
                     self.last_click_like_index = index - 1
                     # 点赞数量更新，本场直播的总点赞数量
                     print(f' 本次直播点赞数量超过 {LIKE_NUMS[index - 1]} 次，达到 {data.click_count} 次')
-                    voice_text = VOICE_TEXT["like_total"].format(limit_num=LIKE_NUMS[index - 1],
-                                                                 click_count=data.click_count)
-                    speech(voice_text)
+                    
+                    if (len(VOICE_TEXT["like_total"]) > 0):
+                        voice_text = VOICE_TEXT["like_total"].format(limit_num=LIKE_NUMS[index - 1],
+                                                                    click_count=data.click_count)
+                        speech(voice_text)
                     return
                 elif index <= 0:
                     break
@@ -271,6 +298,7 @@ class MyHandler(blivedm.BaseHandler):  # 类变量，将被所有类的实例共
 
     def _on_super_chat(self, client: blivedm.BLiveClient, message: web_models.SuperChatMessage):
         print(f' 醒目留言 ¥{message.price} {message.uname}：{message.message}')
+        speech(f' 醒目留言 {message.uname}说：{message.message}')
 
 
 def split_and_reassemble(number):
